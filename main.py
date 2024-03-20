cb90efda26a55cbb756e23e1a34e20a4791aedda
Rahul Pradhan <pradhanrahul277@gmail.com>
2024-03-20 19:01:57 +0530

    Added hand gesture for ppt slide

--- main.py
+++ main.py
@@ -90,19 +90,13 @@ while True:
                     annotationNumber = -1
                     annotationStart = False
 
-            ## gesture 3 - pointer display
+            # gesture 3 - pointer display
                     
             if fingers == [0, 1, 1, 0, 0]:  # Pointer gesture
                 indexFingerTip = lmList[8]  # Index Finger Tip position
                 cv2.circle(img, (indexFingerTip[0], indexFingerTip[1]), 15, (0, 0, 255), cv2.FILLED)
               
-            ## gesture 4 - draw
-            
-            if fingers == [0, 1, 0, 0]:  # Drawing Gesture
-                indexFingerTip = lmList[8]  # Index Finger Tip position
-                drawing_points.append(indexFingerTip)
-                for i in range(1, len(drawing_points)):
-                    cv2.line(img, (drawing_points[i-1][0], drawing_points[i-1][1]), (drawing_points[i][0], drawing_points[i][1]), (255, 0, 0), 15)
+            # gesture 4 - draw
             
             # gesture 5 - erase
                
@@ -122,11 +116,6 @@ while True:
                 cv2.line(imgCurrent, annotation[j - 1], annotation[j], (0, 0, 200), 12)
  
     cv2.imshow("Image", img)
-
-    ## Bring the webcam to the top of presentation
-
-    window = gw.getWindowsWithTitle('Image')[0]
-    window.activate()
     
     key = cv2.waitKey(1)
     if key == ord('q'):
